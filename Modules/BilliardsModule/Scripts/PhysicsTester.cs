e53049304a6e6e6a8cd5d1a2f5e846696648f9a0
Sacchan-VRC <67171402+Sacchan-VRC@users.noreply.github.com>
2024-03-14 23:11:24 +0000

    stop PhysicsTester.cs from running because it's unused and was erroring

--- Modules/BilliardsModule/Scripts/PhysicsTester.cs
+++ Modules/BilliardsModule/Scripts/PhysicsTester.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+// [ExecuteInEditMode] // commented to prevent error appearing in console, as this script is unused
 public class PhysicsTester : MonoBehaviour
 {
     public Texture2D tex;
