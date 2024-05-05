9e87b097157d72b79fb9494755853993da08b71b
Sacchan-VRC <67171402+Sacchan-VRC@users.noreply.github.com>
2024-05-05 17:38:59 +0100

    add missing eijis loader prefab+code

---
+++ eijis/UdonScripts/BilliardsLoadMenu.cs
@@ -0,0 +1,58 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class BilliardsLoadMenu : UdonSharpBehaviour
+{
+    public BilliardsModule billiardsModule;
+    public InputField inputField;
+
+    public void OnSaveButtonPushed()
+    {
+        if (ReferenceEquals(null, billiardsModule))
+        {
+            Debug.Log("BilliardsSaveLoad::OnSaveButtonPushed() billiardsModule property is not set !");
+            return;
+        }
+
+        if (ReferenceEquals(null, inputField))
+        {
+            Debug.Log("BilliardsSaveLoad::OnSaveButtonPushed() inputField property is not set !");
+            return;
+        }
+
+        inputField.text = billiardsModule._SerializeGameState();
+    }
+
+    public void OnLoadButtonPushed()
+    {
+        if (ReferenceEquals(null, billiardsModule))
+        {
+            Debug.Log("BilliardsSaveLoad::OnSaveButtonPushed() billiardsModule property is not set !");
+            return;
+        }
+
+        if (ReferenceEquals(null, inputField))
+        {
+            Debug.Log("BilliardsSaveLoad::OnSaveButtonPushed() inputField property is not set !");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(inputField.text))
+        {
+            return;
+        }
+
+        if (!billiardsModule.isPlayer)
+        {
+            return;
+        }
+
+        billiardsModule._LoadSerializedGameState(inputField.text);
+
+    }
+}
